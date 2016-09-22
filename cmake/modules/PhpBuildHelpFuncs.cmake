9bf81d70cfceee87a72adc01211bc8d27a80737f
softboy <zzu_softboy@163.com>
2016-09-22 22:37:17 +0800

    增加帮助函数的原型

--- cmake/modules/PhpBuildHelpFuncs.cmake
+++ cmake/modules/PhpBuildHelpFuncs.cmake
@@ -4,3 +4,39 @@ function(php_update_compile_flags name)
    
 endfunction()
 
+function(php_add_symbol_exports target_name export_file)
+   
+endfunction()
+
+function(php_add_link_opts target_name)
+   
+endfunction()
+
+function(php_set_output_directory target)
+   
+endfunction()
+
+function(php_add_library name)
+   
+endfunction()
+
+function(add_unittest test_suite test_name)
+endfunction()
+
+macro(add_php_library name)
+   
+endmacro()
+
+macro(add_php_loadable_module)
+   
+endmacro()
+
+macro(add_php_executable name)
+   
+endmacro()
+
+function(php_export_executable_symbols target)
+endfunction()
+
+function(php_externalize_debuginfo name)
+endfunction()
---
+++ cmake/modules/PhpUtilFuncs.cmake
@@ -0,0 +1,9 @@
+#这个文件定义一些常用的工具类
+
+function(php_install_library_symlink name dest type)
+   
+endfunction()
+
+function(php_install_symlink name dest)
+   
+endfunction()
