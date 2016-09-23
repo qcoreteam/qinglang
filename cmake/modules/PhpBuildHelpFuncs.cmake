949fef10b0e7468487d46097f196e2132a50b0ba
softboy <zzu_softboy@163.com>
2016-09-23 10:02:43 +0800

    实现宏PHP_M_ADD_DEFINITIONS

--- cmake/modules/PhpBuildHelpFuncs.cmake
+++ cmake/modules/PhpBuildHelpFuncs.cmake
@@ -19,9 +19,10 @@ endfunction()
 function(php_add_library name)
    
 endfunction()
+
 # 增加一个单元测试
-# test_suite 
-# test_name
+# test_suite 测试集合名称，通常是一个大的模块放在一起然后给一个名字
+# test_name 测试集合里面的单个测试名字
 function(add_unittest test_suite test_name)
    
 endfunction()
@@ -34,18 +35,33 @@ function(php_externalize_debuginfo name)
 endfunction()
 
 #相关宏定义
-macro(PHP_ADD_LIBRARY name)
+macro(PHP_M_ADD_LIBRARY name)
    
 endmacro()
 
-macro(PHP_ADD_LOADABLE_MODULE)
+macro(PHP_M_ADD_LOADABLE_MODULE)
    
 endmacro()
 
-macro(PHP_ADD_EXECUTABLE name)
+macro(PHP_M_ADD_EXECUTABLE name)
    
 endmacro()
 
-macro(PHP_ADD_DEFINITIONS)
-   
+# cmake系统中没有一种方法获取当前已经添加的编译器定义，这个宏我们自己实现相关的功能
+# 参数是跟add_definitions一致
+macro(PHP_M_ADD_DEFINITIONS)
+   foreach(arg ${ARGN})
+      if(DEFINED PHP_DEFINITIONS)
+         set(PHP_DEFINITIONS "${PHP_DEFINITIONS} ${arg}")
+      elseif()
+         set(PHP_DEFINITIONS ${arg})
+      endif(DEFINED PHP_DEFINITIONS)
+   endforeach()
+   add_definitions(${ARGN})
+endmacro(PHP_M_ADD_DEFINITIONS)
+
+macro(PHP_M_ADD_TARGET target_name)
+endmacro()
+
+macro(PHP_M_ADD_SUBDIRECTORY project type name)
 endmacro()
