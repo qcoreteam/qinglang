5c51f241afac4a9c50a45f0ce1362fe120464145
softboy <zzu_softboy@163.com>
2016-09-23 09:26:43 +0800

    增加单元测试的cmake脚本

--- cmake/modules/PhpBuildHelpFuncs.cmake
+++ cmake/modules/PhpBuildHelpFuncs.cmake
@@ -1,5 +1,5 @@
-#这个文件定义一些跟编译相关的帮助函数
-
+# 这个文件定义一些跟编译相关的帮助函数
+# 通用函数定义
 function(php_update_compile_flags name)
    
 endfunction()
@@ -19,24 +19,33 @@ endfunction()
 function(php_add_library name)
    
 endfunction()
-
+# 增加一个单元测试
+# test_suite 
+# test_name
 function(add_unittest test_suite test_name)
+   
 endfunction()
 
-macro(add_php_library name)
-   
-endmacro()
-
-macro(add_php_loadable_module)
-   
-endmacro()
-
-macro(add_php_executable name)
-   
-endmacro()
-
 function(php_export_executable_symbols target)
+   
 endfunction()
 
 function(php_externalize_debuginfo name)
 endfunction()
+
+#相关宏定义
+macro(PHP_ADD_LIBRARY name)
+   
+endmacro()
+
+macro(PHP_ADD_LOADABLE_MODULE)
+   
+endmacro()
+
+macro(PHP_ADD_EXECUTABLE name)
+   
+endmacro()
+
+macro(PHP_ADD_DEFINITIONS)
+   
+endmacro()
--- cmake/modules/PhpUtilFuncs.cmake
+++ cmake/modules/PhpUtilFuncs.cmake
@@ -7,3 +7,21 @@ endfunction()
 function(php_install_symlink name dest)
    
 endfunction()
+
+function(php_replace_compiler_option var old new)
+endfunction()
+
+function(php_add_header_files_for_glob hdrs_out glob)
+endfunction()
+
+function(php_find_all_header_files hdrs_out additional_headerdirs)
+endfunction()
+
+function(php_process_sources out_var)
+endfunction()
+
+function(php_check_source_file_list)
+endfunction()
+
+macro(php_add_td_sources srcs)
+endmacro()
