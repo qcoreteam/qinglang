072511424edac8838383b9e2155ad1fbddcb068c
softboy <zzu_softboy@163.com>
2016-09-27 08:21:01 +0800

    重构include架构

--- src/kernel/base/files_register.cmake
+++ src/kernel/base/files_register.cmake
@@ -1,7 +1,3 @@
 set(BASE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/base)
 set(PHP_KERNEL_LIB_SRCS  ${PHP_KERNEL_LIB_SRCS}
-   ${BASE_DIR}/global.h
-   ${BASE_DIR}/global.cpp
-   ${BASE_DIR}/system_detection.h
-   ${BASE_DIR}/processor_detection.h
-   ${BASE_DIR}/compiler_detection.h)
+   ${BASE_DIR}/global.cpp)
